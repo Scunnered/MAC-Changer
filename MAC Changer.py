498a15e1f6b0322789283521d5b2eca5469a4413
Ross Morrison <44169316+Scunnered@users.noreply.github.com>
2020-06-03 16:42:03 +0100

    Run program in Terminal
    -python MAC_changer.py
    (To check if it has worked run command)
    -ifconfig wlan0
    (At this stage the mac address should be listed as 00:11:22:33:44:77)

--- MAC Changer.py
+++ MAC Changer.py
@@ -3,10 +3,10 @@
 import subprocess
 
 interface = "wlan0"
-new_mac = "00:11:22:33:44:77" #Diffrent from last time
+new_mac = "00:11:22:33:44:77"
 
-print("[+] Changing MAC address for " + interface + " to " + new_mac)
+print("[+] Changing MAC address for " + interface + " to " + new_mac")
 
-# subprocess.call("ifconfig wlan0 down", shell=True)
-# subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66", shell=True) #set MAC Address
-# subprocess.call("ifconfig wlan0 up", shell=True)
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac", shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
