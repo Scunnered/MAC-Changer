1eae3b910f67d5e493fd4fd3b5c4a2595401b19b
Ross Morrison <44169316+Scunnered@users.noreply.github.com>
2020-06-03 16:32:44 +0100

    Adding Variables & Strings - new mac = MAC change
    Will output string in terminal. Will show MAC ending in 77

--- MAC Changer.py
+++ MAC Changer.py
@@ -3,8 +3,9 @@
 import subprocess
 
 interface = "wlan0"
+new_mac = "00:11:22:33:44:77" #Diffrent from last time
 
-print("[+] Changing MAC address for " + interface)
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
 # subprocess.call("ifconfig wlan0 down", shell=True)
 # subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66", shell=True) #set MAC Address
