79e0d13c3b02f640b6b0c1525690a4022efc56fa
Ross Morrison <44169316+Scunnered@users.noreply.github.com>
2020-06-03 16:18:07 +0100

    To run from Terminal:
    Navigate to projects location on KALI
    
    Enter in terminal:
    - cd PycharmProjects/MAC_Changer/ 
    - ls
    - python MAC_Changer.py
    (no errors)
    - ifconfig wlan0
    (here you will be able to see if the MAC address got changed to 00:11:22:33:44:66)
    
    All three system commands have been run.

--- MAC Changer.py
+++ MAC Changer.py
@@ -5,3 +5,4 @@ import subprocess
 subprocess.call("ifconfig wlan0 down", shell=True)
 subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66", shell=True) #set MAC Address
 subprocess.call("ifconfig wlan0 up", shell=True)
+#Can be run in PyCharm or in Terminal
