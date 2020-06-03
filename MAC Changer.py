6323fa3a9285ebe5e6ba1bbf78b6f537095ca6f3
Ross Morrison <44169316+Scunnered@users.noreply.github.com>
2020-06-03 20:48:28 +0100

    Handling Command-line Arguments
    Parser for help, --interface and -i added.

--- MAC Changer.py
+++ MAC Changer.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+
+parser.parse_args()
 
 interface = input("interface > ")
 new_mac = input("New MAC > ")
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac")
-
-#subprocess.call("ifconfig " + interface + " down", shell=True)
-#subprocess.call("ifconfig " + interface + " hw ether " + new_mac", shell=True)
-#subprocess.call("ifconfig " + interface + " up", shell=True)
 
 subprocess.call(["ifconfig", interface, "down"])
 subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
