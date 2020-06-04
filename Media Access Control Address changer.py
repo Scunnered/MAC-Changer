7d6d141dadf0d92b83530e13dfc0c1117ee36e76
Ross Morrison <44169316+Scunnered@users.noreply.github.com>
2020-06-04 15:26:23 +0100

    Implementing Algorithm to check if MAC changed
    Comments also added

--- Media Access Control Address changer.py
+++ Media Access Control Address changer.py
@@ -5,9 +5,9 @@ import optparse
 import re
 
 
-def get_arguments():
+def get_arguments(): 
   parser = optparse.OptionParser()
-  parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+  parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address") #terminal side promts to guide user
   parser.add_option("-m", "--mac", dest="new_MAC", help="New MAC address")
   (options, arguments) = return parser.parse_args()
   if not options.interface: 
@@ -19,30 +19,30 @@ def get_arguments():
   
 def change_mac(interface, new_mac):
   print("[+] Changing MAC address for " + interface + " to " + new_mac")
-  subprocess.call(["ifconfig", interface, "down"])
-  subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-  subprocess.call(["ifconfig", interface, "up"])
+  subprocess.call(["ifconfig", interface, "down"]) #closes MAC instances
+  subprocess.call(["ifconfig", interface, "hw", "ether", new_mac]) #enables edits of MAC address
+  subprocess.call(["ifconfig", interface, "up"]) #establishes MAC address with custom MAC
   
         
 def get_current_mac(interface):
     ifconfig_result = subprocess.check_output(["ifconfig", interface])        
-    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result) #rule created on https://pythex.org/
 
     if mac_address_search_result:
       return mac_address_search_result.group(0)
     else:
-      print("MAC address unreadable.")
+      print("MAC address unreadable.") #if user specifed lo as an interface
 
 
 options = get_arguments() 
         
 current_mac = get_current_mac(options.interface)
-print("Current MAC = " + str(current_mac))
+print("Current MAC = " + str(current_mac)) # returns current MAC
 
 change_mac(options.interface, options.new_mac)
 
 current_mac = get_current_mac(options.interface)
 if current_mac == option.new_mac:
-      print("Mac address was successfully changed to " + current_mac)
+      print("Mac address was successfully changed to " + current_mac) #confirms if MAC has been changed
 else:
-      print("MAC address was not changed.")
+      print("MAC address was not changed.") #confirms if MAC has not been changed
