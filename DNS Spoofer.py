f0565b35da4d68a81111afb236b4a1e05e5c07eb
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-06-15 19:59:19 +0100

    Converting Packets to Scapy Packets

--- DNS Spoofer.py
+++ DNS Spoofer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import netfilterqueue
+import scapy.all
 
 def process_packet(packet):
-    print(packet)
-    packet.drop()
+    scapy_packet = scapy.IP(packet.get_payload())
+    print(scapy_packet.show())
+    packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
 queue.bind(0, process_packet)
