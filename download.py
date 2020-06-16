9a09eff4df650c976dc647a2bef3544acd13a735
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-06-16 14:56:21 +0100

    Writing Files on Disk

--- download.py
+++ download.py
@@ -3,6 +3,8 @@ import requests
 
 def download(url):
     get_response = request.get(url)
-    print(get_response.content)
+    file_name = url.split("-/-")[-1]
+    with open(file_name, "wb") as out_file:
+        out_file.write(get_response.content)
 
 download("https://i.ytimg.com/vi/-1qju6V1jLM/maxresdefault.jpg")
