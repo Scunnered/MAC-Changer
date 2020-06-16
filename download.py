1691df9fee0ef939f528afb7da8b5ac6910d7822
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-06-16 14:51:12 +0100

    Downloading Files From script

--- download.py
+++ download.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python
-#Obligatory "CJ" quote
+import requests
+
+def download(url):
+    get_response = request.get(url)
+    print(get_response.content)
+
+download("https://i.ytimg.com/vi/-1qju6V1jLM/maxresdefault.jpg")
