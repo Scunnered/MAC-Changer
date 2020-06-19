95957da6ee918628e7b5bb4dfbd2ee6a3f32df76
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-06-19 14:56:12 +0100

    Recursively Discovering All Paths On a Target Website
    Goal > recursively list all links starting from a base URL
    -Read page html
    -extract all links
    -Repeat for each new link that is not already in the list

--- crawler.py
+++ crawler.py
@@ -5,7 +5,7 @@ import re
 import urlparse
 
 
-target_url = "https://zsecurity.org/product/realtek-ar8812au-2-4-5-ghz-usb-wireless-adapter/"
+target_url = "http://10.0.2.20/mutillidae"
 target_links = []
 
 
@@ -14,11 +14,17 @@ def extract_links_from(url)
     return re.findall('(?:href=")(.+?)"', response.content)
 
 
-href_links = extract_links_from(target_url)
-for link in href_links:
-    link = urlparse.urljoin(target_url, link)
+def crawl(url):
+    href_links = extract_links_from(url)
+    for link in href_links:
+        link = urlparse.urljoint(url, link)
 
+        if '#' in link:
+            link=link.split("#")[0]
 
-    if target_url in link and link not in target_links:
-        target_links.append(link)
-        print(link)
+        if target_url in link and link not in target_links:
+            target_links.append(link)
+            print(link)
+            crawl(link)
+
+crawl(target_url)
