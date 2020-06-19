1261e47ed3b0f4948dfa2d1a61ac60353a7dd37e
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-06-19 13:37:53 +0100

    Extracting Unique Links & Storing Them In a List

--- crawler.py
+++ crawler.py
@@ -5,7 +5,8 @@ import re
 import urlparse
 
 
-target_url = "https://zsecurity.org"
+target_url = "https://zsecurity.org/product/realtek-ar8812au-2-4-5-ghz-usb-wireless-adapter/"
+target_links = []
 
 def extract_links_from(url)
     response = request.get(url)
@@ -15,5 +16,6 @@ href_links = extract_links_from(target_url)
 for link in href_links:
     link = urlparse.urljoin(target_url, link)
 
-    if target_url in link:
+    if target_url in link and link not in target_links:
+        target_links.append(link)
         print(link)
