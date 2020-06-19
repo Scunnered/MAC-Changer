e8cab2c1383edea5907e7ce50a222aeba230af1d
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-06-19 13:07:57 +0100

    Reading Response Content
    subdomains
    - Domain before the actual domain name.
    - Part of the main domain

--- crawler.py
+++ crawler.py
@@ -8,12 +8,7 @@ def request(url):
     except requests.exceptions.ConnectionError:
         pass
 
-target_url = "10.0.2.20/mutillidae/"
+target_url = "zsecurity.org"
 
-with open("/root/Downloads/common.txt", "r") as wordlist_file:
-    for line in wordlist_file:
-        word = line.strip()
-        test_url = target_url + "/" + word
-        response = request(test_url)
-        if response:
-            print("-/- Discovered URL " + test_url)
+response = request(target_url)
+print(response.content)
