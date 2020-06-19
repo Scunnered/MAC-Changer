5d5afb79861f59f73903ed0d323c1743616986f5
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-06-19 13:44:33 +0100

    Extracting Links & Storing Them In a List

--- crawler.py
+++ crawler.py
@@ -8,13 +8,16 @@ import urlparse
 target_url = "https://zsecurity.org/product/realtek-ar8812au-2-4-5-ghz-usb-wireless-adapter/"
 target_links = []
 
+
 def extract_links_from(url)
     response = request.get(url)
     return re.findall('(?:href=")(.+?)"', response.content)
 
+
 href_links = extract_links_from(target_url)
 for link in href_links:
     link = urlparse.urljoin(target_url, link)
+
 
     if target_url in link and link not in target_links:
         target_links.append(link)
