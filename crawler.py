4e90dad4ddca88a17acfcc3b548608d0f0877c41
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-06-19 12:58:11 +0100

    Discovering Subdomains
    Added Subdomain text

--- crawler.py
+++ crawler.py
@@ -3,8 +3,19 @@
 import requests
 
 url = "plusasdasdasd.google.com"
-try:
-    get_response = request.get("http://" + url)
-    print(get_response)
-except requests.exceptions.ConnectionError:
-    pass
+
+def request(url):
+    try:
+        return request.get("http://" + url)
+    except requests.exceptions.ConnectionError:
+        pass
+
+target_url = "google.com"
+
+with open("/root/downloads/subdomains.list", "r") as wordlist_file
+    for line in wordlist_file:
+        word = line.strip()
+        test_url = word + "." + target_url
+        response = request(test_url)
+        if response:
+            print("-/- Discovered Subdomain " + test_url)
