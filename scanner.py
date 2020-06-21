956c461cf5ff3d3115ed466a726e42df7fd2f954
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-06-21 18:42:19 +0100

    Extracting & Submitting Forms Automatically

--- scanner.py
+++ scanner.py
@@ -3,6 +3,7 @@
 import requests
 import re
 import urlparse
+from BeautifulSoup import BeautifulSoup
 
 class Scanner:
     def _init_(self, url, ignore_links):
@@ -29,3 +30,27 @@ class Scanner:
                 self.target_links.append(link)
                 print(link)
                 self.crawl(link)
+
+    def extract_forms(self, url):
+        response = self.session.get(url)
+        parsed_html = BeautifulSoup(response.content)
+        return parsed_html.findAll("from")
+
+    def submit_form(self, form, value, url):
+        action = form.get("action")
+        post_url = urlparse.urljoin(url, action)
+        method = form.get("method")
+
+        inputs_list = form.findAll("input")
+        post_data = {}
+        for input in inputs_list:
+            input_name = input.get("name")
+            input_type = input.get("type")
+            input_value = input.get("value")
+            if input_type == "text":
+                input_value = "test"
+
+            post_data[input_name] = input_value
+        if method == "post":
+            return self.session.post(post_url, data=post_data)
+        return self.session.get(post_url, params=post_data)
